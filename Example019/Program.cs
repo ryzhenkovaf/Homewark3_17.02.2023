b7217c95149dd58db1b76631d5f0496462259f7a
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-02-17 21:24:28 +0700

    add 19

--- Example019/Program.cs
+++ Example019/Program.cs
@@ -6,16 +6,25 @@
 
 Console.Clear();
 Console.Write("Введите пятизначное число: ");
-string number = Console.ReadLine();
-int len = number.Length;
-if (len == 5)
-{ 
-    if (number[2] == number[4] && number[1] == number[5])
+int number = int.Parse(Console.ReadLine());
+int a1 = number / 10000;
+int a2 = number / 1000 - (a1*10);
+int a3 = number / 100 - ((a1*100)+(a2*10));
+int a4 = number / 10 - ((a1*1000)+(a2*100)+(a3*10));
+int a5 = number % 10;
+
+if (number >= 10000 && number < 100000)
     {
-        Console.Write($"{number} палиндром");
+        if (a2 == a4 && a1 == a5)
+        {
+            Console.WriteLine($"{number} - палиндром");
+        }
+        else
+        {
+            Console.WriteLine($"{number} - НЕ палиндром");
+        }
     }
-    else
-    {
-        Console.Write($"{number} не палиндром");
-    }
+else
+{
+    Console.WriteLine("Введено не пятизначное число ");
 }
