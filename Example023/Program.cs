10716b0f9ef5a736af8bed7cc2d66c7598ef1f20
Aleksei Ryzhenkov <ryzhenkovaf@gmail.com>
2023-02-17 20:33:21 +0700

    add 23

---
+++ Example023/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int[] result = new int[N];
+for (int i = 1; i <= N; i++)
+    result [i-1] = i * i * i;
+for (int i = 0; i < N; i++)
+    Console.Write($"{result[i]}");
